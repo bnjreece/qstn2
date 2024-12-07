9f76c80f46c0d933f0f3b44e7eac7a4db14dbdb9
Your Name <your.email@example.com>
2024-12-06 17:10:43 -0700

    fix: update import path for useDebounce hook

--- app/components/form/TextInput.tsx
+++ app/components/form/TextInput.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useDebounce } from '~/hooks/useDebounce';
+import { useDebounce } from '../../hooks/useDebounce';
 
 interface TextInputProps {
   value: string;

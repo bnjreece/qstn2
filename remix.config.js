876b71cb4d442d53d6cf0eba59a23a0a9840ff85
Your Name <your.email@example.com>
2024-12-09 18:22:01 -0700

    fix: add heroicons to server bundle config

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  serverDependenciesToBundle: ["@heroicons/react"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+}; 
